9f4c2751b12cb3c1ce7f0b686252441d7f5be501
Salaudeen Adebayo <profbayo4real@gmail.com>
2022-03-09 19:46:06 +0000

    First commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='EDSA example python package',
+    long_description=open('README.md').read(),
+    install_requires=['numpy'],
+    url='https://github.com/prof4321/mypackage',
+    author='Salaudeen Adebayo',
+    author_email='profbayo4real@gmail.com'
+)
